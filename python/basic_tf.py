8a8bed3366c3c201ad7d619334de9e8c3c4baf74
lee-iv <36719127+lee-iv@users.noreply.github.com>
2020-06-26 00:05:51 -0700

    setup basic_tf.py to simulate step response of 1st order lag system

--- python/basic_tf.py
+++ python/basic_tf.py
@@ -1,2 +1,19 @@
 import numpy as np
+import matplotlib.pyplot as plt
 import control
+
+# Create transfer function
+s = control.tf('s')
+tf1 = 1/(1+s)
+
+# Simulate step response
+T = np.linspace(0,10,100)
+X0 = 0
+T, yout = control.step_response(tf1, T, X0)
+
+# Plot
+fig,ax = plt.subplots(1,1,sharex=True)
+ax.plot(T,yout)
+ax.grid()
+
+plt.show()
